c5227993e571d66eb864a9fd5a307c3595676759
Asher Plotsky <asher.plotsky@gmail.com>
2026-04-29 14:05:06 -0700

    Program 4

--- Program4.c
+++ Program4.c
@@ -2,56 +2,76 @@
 //This code works in SEGGER which the project is intended for but not in CLion
 int main(void)
 {
+    //region First Section
     int arr[] = {1, 2, 3, 4, 5};
-    int rotate = 3;
-    int newValue = 8;
-    int byte = 4;
-    int newArray[sizeof(arr)/sizeof(arr[0]) + 1];
+    int newValue = 6;
+
     int size = sizeof(arr)/sizeof(arr[0]);
-    int newSize = sizeof(arr)/sizeof(arr[0]) + 1;
+    int reverseArray[size];
+    int reverseSize = size - 1;
+    int newArray[size + 1];
+    int newSize = sizeof(newArray)/sizeof(newArray[0]);
+    for (int i = 0; i < size; i++){
+        printf("%d", arr[i]);
+        if (i+1 != size){
+            printf(", ");
+        }
+    }
+
     asm volatile (
         "mov r4, %[arr]\n\t"
         "mov r5, %[newArray]\n\t"
-        "mov r6, %[rotate]\n\t"
-        "mov r7, %[newValue]\n\t"
-        "mov r8, %[byte]\n\t"
-        "mov r9, %[newSize]\n\t"
-        "mov r11, %[size]\n\t"
-        "ldr r10, =0x20001000\n\t"
-        "sub r9, r9, #1\n\t"
+        "mov r6, %[newValue]\n\t"
+        "mov r7, %[size]\n\t"
+        "mov r10, %[reverseArray]\n\t"
+        "mov r11, %[reverseSize]\n\t"
+        "mov r9, #4\n\t"
 
-        "Rotate: \n\t"
-        "cmp r6, #0\n\t"
-        "beq Add\n\t"
-        "sub r6, r6, #1\n\t"
-        "add r12, r4, #0\n\t"
-        "add r4, r4, r8\n\t"
-        ""
+        "mul r11, r11, r9\n\t"
+        "add r10, r10, r11\n\t"
+        "Copy: \n\t"
+        "ldr r8, [r4]\n\t"
+        "str r8, [r5]\n\t"
+        "str r8, [r10]\n\t"
+        "add r4, r4, r9\n\t"
+        "add r5, r5, r9\n\t"
+        "sub r10, r10, r9\n\t"
+        "sub r7, r7, #1\n\t"
+        "cmp r7, #0\n\t"
+        "bne Copy \n\t"
 
-        "b Rotate"
+        "str r6, [r5]\n\t"
 
-        "Add: \n\t"
-        "mul r9, r9, r8\n\t"
-        "add r9, r5, r9\n\t"
-        "str r7, [r9]\n\t"
-
-        "End: \n\t"
-        "mov %[newArray], r5\n\t"
-
-        : [arr] "=r" (arr), [newArray] "=r" (newArray), [newValue] "=r" (newValue)
-        : [newSize] "r" (newSize), [byte] "r" (byte), [rotate] "r" (rotate), [size] "r" (size)
-        : "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12"
+        :
+        :[arr] "r" (arr), [newArray] "r" (newArray), [newValue] "r" (newValue), [size] "r" (size), [reverseArray] "r" (reverseArray), [reverseSize] "r" (reverseSize)
+        : "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "memory"
     );
+    printf("\nExtended Array: \n");
     for (int i = 0; i < newSize; i++){
         printf("%d", newArray[i]);
         if (i+1 != newSize){
             printf(", ");
         }
     }
+    printf("\nReverse Array: \n");
+    for (int i = 0; i < size; i++){
+        printf("%d", reverseArray[i]);
+        if (i+1 != size){
+            printf(", ");
+        }
+    }
     printf("\n");
+    //endregion
+    //region Second Section
+
     asm volatile (
         ""
+
+        :
+        :
+        :
     );
 
+    //endregion
     return 0;
 }
